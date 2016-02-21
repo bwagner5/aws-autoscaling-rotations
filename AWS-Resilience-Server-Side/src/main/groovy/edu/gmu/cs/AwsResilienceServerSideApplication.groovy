7fcff96543306f59b8446b576a3c90f698edf7b4
Brandon Wagner <brandon.wagner@teamaol.com>
2016-02-21 16:08:20 -0500

    split apps into client and server side

---
+++ AWS-Resilience-Server-Side/src/main/groovy/edu/gmu/cs/AwsResilienceServerSideApplication.groovy
@@ -0,0 +1,13 @@
+package edu.gmu.cs
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AwsResilienceServerSideApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run AwsResilienceServerSideApplication, args
+	}
+
+}
